7d5b4dcad0a42a0af8b256da94083c904bfaa61e
CoderJackLiu <596770421@qq.com>
2023-07-24 17:23:56 +0800

    添加自定义设置选项；添加参数化，配置第二个视口；交互正常，展示正常；

---
+++ Plugins/RuntimeEditor/Source/RuntimeEditor/Private/Viewports/RE_ViewportManager.cpp
@@ -0,0 +1,28 @@
+﻿$COPYRIGHT_LINE$
+
+
+$MY_HEADER_INCLUDE_DIRECTIVE$
+
+
+// Sets default values
+$PREFIX$$UNPREFIXED_CLASS_NAME$::$PREFIX$$UNPREFIXED_CLASS_NAME$()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void $PREFIX$$UNPREFIXED_CLASS_NAME$::BeginPlay()
+{
+	Super::BeginPlay();
+	$END$
+}
+
+// Called every frame
+void $PREFIX$$UNPREFIXED_CLASS_NAME$::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
